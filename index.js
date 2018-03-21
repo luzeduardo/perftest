62b7b1b85e005ae9cb46af159957bdcb56281376
Eduardo <luz.eduardo@gmail.com>
2018-03-20 22:13:12 -0300

    perf test with pino logger

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+const responseTime = require('response-time');
+const express = require('express');
+const winston = require('winston');
+const pino = require('pino')()
+const PORT = process.env.port || 3000;
+const server = express();
+
+
+server.use(responseTime());
+server.use('/', (req, res) => {
+  // console.time()
+  res.send('Data')
+  // console.timeEnd()
+});
+
+pino.info('request test perf with pino logger')
+
+server.listen(PORT, () => {
+  console.log('Listem to them')
+});
