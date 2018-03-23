794491737fe8e7512543a21ea6323f3b69269d7b
Eduardo <luz.eduardo@gmail.com>
2018-03-23 06:52:16 -0300

    iterable protocol async loops

---
+++ calculations.js
@@ -0,0 +1,23 @@
+function memoize(fn) {
+  const cache = {}
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
+    const res = fn.apply(this, args)
+    cache[args] = res
+    console.log(res)    
+    return res
+  }
+}
+function slowFib(n) {
+  if (n < 2) {
+    return n
+  }
+  return fib(n - 1) + fib(n - 2)
+}
+const fib = memoize(slowFib)
+
+export {
+  fib
+}
---
+++ iterableprotocol.js
@@ -0,0 +1,17 @@
+const delay = _ => {
+  return new Promise(resolve => setTimeout(resolve, 500));
+}
+
+const delayedLog = async (item) => {
+  await delay();
+  console.log(item)
+}
+
+const paralleliterator = async (array) => {
+  const promises = array.map(delayedLog);
+  return await Promise.all(promises);
+}
+
+export {
+  paralleliterator
+}
